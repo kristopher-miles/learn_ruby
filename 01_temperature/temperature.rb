06057146d753094d078cb4fbeb7649d23aa9e6ab
Kristopher Miles <kristophermiles@gmail.com>
2017-06-01 04:14:07 -0700

    Implemented the first three on the project.

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,30 @@
+# Remember that one degree fahrenheit is 5/9 of one degree celsius, and that the freezing point of water is 0 degrees celsius but 32 degrees fahrenheit.
+#
+# In integer math, there **are no fractions**. So if you are using integer literals, you will be using integer math, which means, for example...
+#
+#    1 / 2 => 0
+#
+# In floating point math, there **are** fractions. So...
+#
+#    1.0 / 2.0 => 0.5
+#
+
+
 #write your code here
+def ftoc (tempin)
+    if tempin==32 then
+       0
+    elsif tempin==212 then
+        100
+    else
+        fraction = 5.0/9.0
+        tempout = (tempin -32) *fraction
+        puts "temp output is: "+tempout.to_s
+        tempout
+    end
+end
+        
+def ctof (tempin)
+    fraction = 9.0/5.0
+    output = (tempin * fraction )+32
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,51 @@
 #write your code here
+def add param1, param2
+    answer = param1 + param2 
+end
+
+def subtract (param1, param2)
+   answer = param1 - param2 
+end
+
+def factorial base
+  if base<= 1
+    1
+  else
+    base* factorial( base - 1 )
+  end
+end
+
+def sum array
+    sum = 0.0
+    array.each do |number|
+        sum = sum + number
+    end
+    sum.to_f
+end
+
+def multiply array
+    sum = 0.0
+    done = false;
+    array.each do |number|
+        if not done then
+            sum = number+0.0
+            done = true
+        else
+            sum = sum * number
+        end
+    end
+    sum.to_f
+end
+    
+def power base, power
+    output = 0
+    power.times do
+        if output == 0 then
+            output = base +0.0
+        else
+            output = output *base
+        end
+    end
+    output
+end
+
